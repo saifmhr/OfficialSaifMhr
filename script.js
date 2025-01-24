c71368a8fb0720366baebc79477216261abc76ad
Saifullah Mahar <69516276+saifmhr@users.noreply.github.com>
2025-01-24 09:55:37 +0500

    js file

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for contacting me! I will get back to you soon.');
+});
